32427663321c5586a7596c516190b8e82439d566
Saurabh <saurabhguptajpr@yahoo.in>
2020-11-26 22:14:52 +0530

    Added Memories app

---
+++ server/models/postMessage.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const postSchema = Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+})
+
+var PostMessage = mongoose.model('Memories', postSchema);
+
+export default PostMessage;
